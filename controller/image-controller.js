bf76022ded6f032fdfad00d60090f3cc01c417bd
ankit <ankitkannaujiya666@gmail.com>
2024-12-24 01:46:44 +0530

    add file base url from env

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -1,45 +1,47 @@
-import grid from 'gridfs-stream';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv'
-dotenv.config({path:'../config.env'});
+import grid from "gridfs-stream";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
 
-const BaseUrl=  process.env.BaseUrl ||'http://localhost:5001';
-
+const BaseUrl = process.env.MONGO_URI || "http://localhost:5001";
 // check if our db connected or not
-const isConnected= mongoose.connection;
+const isConnected = mongoose.connection;
 let gridfsBucket;
 let gfs;
-isConnected.once('open',()=>{
-    gridfsBucket=new mongoose.mongo.GridFSBucket(isConnected.db,{
-        bucketName:'fs'
-    });
-    // make readable format via gridfs-stream
-    gfs=grid(isConnected.db, mongoose.mongo);
-    gfs.collection('fs')
-})
+isConnected.once("open", () => {
+  gridfsBucket = new mongoose.mongo.GridFSBucket(isConnected.db, {
+    bucketName: "fs",
+  });
+  // make readable format via gridfs-stream
+  gfs = grid(isConnected.db, mongoose.mongo);
+  gfs.collection("fs");
+});
 
-// for uploading an image file 
-export const fileUpload=(request,response)=>{
-        if (!request.file) {
-         return response.status(404).json({statusCode:404, message:"File not found!"});
-        }
-        
-        const imageURL=`${BaseUrl}/file/${request.file.filename}`
+// for uploading an image file
+export const fileUpload = (request, response) => {
+  if (!request.file) {
+    return response
+      .status(404)
+      .json({ statusCode: 404, message: "File not found!" });
+  }
 
-        return response.status(200).json({ statusCode:200, imageURL: imageURL});
-}
+  const imageURL = `${BaseUrl}/file/${request.file.filename}`;
 
-//for get image from mongoDB 
+  return response.status(200).json({ statusCode: 200, imageURL: imageURL });
+};
 
-export const getImage= async (request,response)=>{
-    try {
-        // to get your image 
-      let file=  await gfs.files.findOne({filename: request.params.filename});
-      // you need to download the image 
-     const readStream= gridfsBucket.openDownloadStream(file._id);
-     readStream.pipe(response);
+//for get image from mongoDB
 
-    } catch (error) {
-        return response.status(500).json({statusCode:500, message:" Internal server error!"})
-    }
-}
+export const getImage = async (request, response) => {
+  try {
+    // to get your image
+    let file = await gfs.files.findOne({ filename: request.params.filename });
+    // you need to download the image
+    const readStream = gridfsBucket.openDownloadStream(file._id);
+    readStream.pipe(response);
+  } catch (error) {
+    return response
+      .status(500)
+      .json({ statusCode: 500, message: " Internal server error!" });
+  }
+};
